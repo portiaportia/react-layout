23e1990135e4999daf37c2c32b9e316fc1973252
portiaportia <portiaplante@gmail.com>
2024-10-31 12:30:14 -0400

    house plans

---
+++ react-layout/src/components/HousePlans.js
@@ -0,0 +1,12 @@
+import {useState, useEffect } from "react";
+import axios from "axios";
+
+const HousePlans = () => {
+    return (
+       <div className="house-plans">
+
+       </div>
+    );
+};
+
+export default HousePlans;
--- react-layout/src/pages/About.js
+++ react-layout/src/pages/About.js
@@ -1,5 +1,5 @@
 import SlideShow from "../components/SlideShow";
-import HousePlan from "../components/HousePlan";
+import HousePlans from "../components/HousePlans";
 
 const About = () => {
     return (
@@ -7,14 +7,7 @@ const About = () => {
             <h3>About</h3>
             <SlideShow />
 
-            <h3>House Plans</h3>
-            <div class="house-plans">
-                <HousePlan 
-                    name="Best House"
-                    size="200sf"
-                    bedrooms="1"
-                    bathrooms="0"/>
-            </div>
+            <HousePlans />
         </>
     );
 };
