1f3924201c6edceb8f45f16d5436d078e90eec47
portiaportia <portiaplante@gmail.com>
2024-10-31 12:23:49 -0400

    stuff

--- react-layout/src/pages/About.js
+++ react-layout/src/pages/About.js
@@ -1,10 +1,20 @@
 import SlideShow from "../components/SlideShow";
+import HousePlan from "../components/HousePlan";
 
 const About = () => {
     return (
         <>
             <h3>About</h3>
             <SlideShow />
+
+            <h3>House Plans</h3>
+            <div class="house-plans">
+                <HousePlan 
+                    name="Best House"
+                    size="200sf"
+                    bedrooms="1"
+                    bathrooms="0"/>
+            </div>
         </>
     );
 };
